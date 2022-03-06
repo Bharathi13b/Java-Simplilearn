5570b870e789c6719070dc24cd4e186ebdcfd417
Bharathi13b <99890778+Bharathi13b@users.noreply.github.com>
2022-03-06 08:14:49 +0530

    Add files via upload

---
+++ Throw_Keyword.java
@@ -0,0 +1,15 @@
+package com.exception;
+
+public class Throw_Keyword {
+	public static void main(String[] args) {
+		System.out.println("WElcome to mphasis bank");
+		int account[]= {101,102,103};
+		String names[]= {"bharathi","Vani","Sushma"};
+		for (int i=0; i <account.length;i++)
+		{
+			System.out.println(account[i]+" "+names[i]);
+		}
+		
+	}
+
+}
