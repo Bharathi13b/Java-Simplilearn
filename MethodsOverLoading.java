beb9a0c85bb06aba7d34899a71fba7b958f86799
Bharathi13b <99890778+Bharathi13b@users.noreply.github.com>
2022-03-06 08:24:33 +0530

    Add files via upload

---
+++ MethodsOverLoading.java
@@ -0,0 +1,21 @@
+package method.com;
+
+public class MethodsOverLoading {
+	public void area(int b,int h)
+    {
+         System.out.println("Area of Triangle : "+(0.5*b*h));
+    }
+    public void area(int r) 
+    {
+         System.out.println("Area of Circle : "+(3.14*r*r));
+    }
+
+    public static void main(String args[])
+   {
+    	MethodsOverLoading ob=new MethodsOverLoading();
+    	
+            ob.area(6,8);
+            ob.area(8);  
+
+}
+}   
